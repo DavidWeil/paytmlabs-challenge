80a9de7052ec2c7568210697fd6297707b4e6f69
David Weil <david.weil@software.dell.com>
2017-03-28 11:39:06 -0400

    The product servlet can send either JSON or HTML
    Depending on the "Accept:" header of the request, the product servlet will now either send JSON or HTML.

--- product/src/main/java/com/example/product/ProductServlet.java
+++ product/src/main/java/com/example/product/ProductServlet.java
@@ -38,26 +38,75 @@ public final class ProductServlet extends HttpServlet {
                 .create();
     }
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
+        // For simplicity we only send JSON if it's specifically requested.
+        String accept = request.getHeader("Accept");
+        boolean sendJSON = "application/json".equals(accept);
+
         String productData = request.getPathInfo();
-        String pageURL = getBaseUrl(request);
         if (productData == null || productData.length() < 2) {
             // Do a general query for a page of data
             String pageNum = request.getParameter("page");
             int page = 1;
             try {
                 page = Integer.parseInt(pageNum);
-            } catch (NumberFormatException e) {
+            }
+            catch (NumberFormatException e) {
                 // We don't care; just use the first page.
             }
-            String targetURL = BASE_URL + "?page=" + page;
-            showListPage(response, pageURL, targetURL);
-        } else {
+
+            ListResponse<Product> results;
+            try {
+                results = getProductList(page);
+            }
+            catch (IOException e) {
+                reportError(response, sendJSON, e.getMessage());
+                return;
+            }
+
+            if (results.getStatus() != 200) {
+                reportError(response, sendJSON, results.getMessage());
+                return;
+            }
+
+            if (sendJSON) {
+                response.setContentType("application/json");
+                gson.toJson(results.getResults(), response.getWriter());
+            }
+            else {
+                String pageURL = getBaseUrl(request);
+                showListPage(response, pageURL, results);
+            }
+        }
+
+        // Send the data for a single product.
+        else {
             // Remove the leading '/'
             String productID = productData.substring(1);
-            String targetURL = BASE_URL + productID;
-            showProductPage(response, pageURL, targetURL);
+
+            SimpleResponse<Product> results;
+            try {
+                results = getProductData(productID);
+            }
+            catch (IOException e) {
+                reportError(response, sendJSON, e.getMessage());
+                return;
+            }
+
+            if (results.getStatus() != 200) {
+                reportError(response, sendJSON, results.getMessage());
+                return;
+            }
+
+            if (sendJSON) {
+                response.setContentType("application/json");
+                gson.toJson(results.getResult(), response.getWriter());
+            }
+            else {
+                showProductPage(response, results);
+            }
         }
     }
 
@@ -69,7 +118,49 @@ public final class ProductServlet extends HttpServlet {
         return scheme + serverName + serverPort + contextPath;
     }
 
-    private void showListPage(HttpServletResponse response, String pageURL, String targetURL)
+    private ListResponse<Product> getProductList(int page) throws IOException {
+
+        String targetURL = BASE_URL + "?page=" + page;
+        HttpURLConnection connection = (HttpURLConnection) new URL(targetURL).openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Authorization", "Token " + API_KEY);
+        connection.connect();
+        int status = connection.getResponseCode();
+        if (status == 200) {
+            try (InputStreamReader reader = new InputStreamReader(connection.getInputStream())) {
+                Type productListType = new TypeToken<ListResponse<Product>>(){}.getType();
+                return gson.fromJson(reader, productListType);
+            }
+        }
+        throw new IOException("Error response: " + status);
+    }
+
+    private void reportError(HttpServletResponse response, boolean sendJSON, String message) throws IOException {
+        if (sendJSON) {
+            response.setContentType("application/json");
+            PrintWriter writer = response.getWriter();
+            writer.print("{\"type\":\"error\",\"message\":\"");
+            writer.print(gson.toJson(message));
+            writer.println("\"}");
+        }
+        else {
+            response.setContentType("text/html");
+            PrintWriter writer = response.getWriter();
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<title>Error</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("Error retrieving data:");
+            writer.println(message);
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
+
+    private void showListPage(HttpServletResponse response, String pageURL, ListResponse<Product> results)
             throws IOException {
 
         response.setContentType("text/html");
@@ -80,6 +171,45 @@ public final class ProductServlet extends HttpServlet {
         writer.println("</head>");
         writer.println("<body>");
 
+        writer.println("<table border=\"0\">");
+        writer.println("<tr>");
+        writer.println("<th>Product ID</th>");
+        writer.println("<th>Name</th>");
+        writer.println("</tr>");
+
+        List<Product> products = results.getResults();
+        for (Product p : products) {
+            writer.println("<tr>");
+            writer.println("<th>" + p.getId() + "</th>");
+            writer.println("<th><a href=\"" + pageURL + '/' + p.getId() + "\">" + p.getName() + "</a></th>");
+            writer.println("</tr>");
+        }
+        writer.println("</table>");
+
+        Pager pager = results.getPager();
+        int current = pager.getCurrentPage();
+        if (current > 1) {
+            writer.println("<a href=\"" + pageURL + "?page=" + (current - 1) + "\">Previous Page</a>");
+        }
+        else {
+            writer.println("Previous Page");
+        }
+        writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+
+        if (current < pager.getLastPage()) {
+            writer.println("<a href=\"" + pageURL + "?page=" + (current + 1) + "\">Next Page</a>");
+        }
+        else {
+            writer.println("Next Page");
+        }
+
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+
+    private SimpleResponse<Product> getProductData(String productID) throws IOException {
+
+        String targetURL = BASE_URL + productID;
         HttpURLConnection connection = (HttpURLConnection) new URL(targetURL).openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Authorization", "Token " + API_KEY);
@@ -87,62 +217,14 @@ public final class ProductServlet extends HttpServlet {
         int status = connection.getResponseCode();
         if (status == 200) {
             try (InputStreamReader reader = new InputStreamReader(connection.getInputStream())) {
-                Type productListType = new TypeToken<ListResponse<Product>>(){}.getType();
-                ListResponse<Product> results = gson.fromJson(reader, productListType);
-
-                if (results.getStatus() != 200) {
-                    writer.println("Request error:");
-                    writer.println(results.getMessage());
-                }
-                else {
-                    writer.println("<table border=\"0\">");
-                    writer.println("<tr>");
-                    writer.println("<th>Product ID</th>");
-                    writer.println("<th>Name</th>");
-                    writer.println("</tr>");
-
-                    List<Product> products = results.getResults();
-                    for (Product p : products) {
-                        writer.println("<tr>");
-                        writer.println("<th>" + p.getId() + "</th>");
-                        writer.println("<th><a href=\"" + pageURL + '/' + p.getId() + "\">" + p.getName() + "</a></th>");
-                        writer.println("</tr>");
-                    }
-                    writer.println("</table>");
-
-                    Pager pager = results.getPager();
-                    int current = pager.getCurrentPage();
-                    if (current > 1) {
-                        writer.println("<a href=\"" + pageURL + "?page=" + (current - 1) + "\">Previous Page</a>");
-                    }
-                    else {
-                        writer.println("Previous Page");
-                    }
-                    writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-
-                    if (current < pager.getLastPage()) {
-                        writer.println("<a href=\"" + pageURL + "?page=" + (current + 1) + "\">Next Page</a>");
-                    }
-                    else {
-                        writer.println("Next Page");
-                    }
-                }
-            }
-            catch (IOException e) {
-                writer.println("Error retrieving data:");
-                writer.println(e.getLocalizedMessage());
+                Type productType = new TypeToken<SimpleResponse<Product>>(){}.getType();
+                return gson.fromJson(reader, productType);
             }
         }
-        else {
-            writer.println("Error connecting to LCBO server:");
-            writer.println(connection.getResponseMessage());
-        }
-
-        writer.println("</body>");
-        writer.println("</html>");
+        throw new IOException("Error response: " + status);
     }
 
-    private void showProductPage(HttpServletResponse response, String pageURL, String targetURL) throws IOException {
+    private void showProductPage(HttpServletResponse response, SimpleResponse<Product> results) throws IOException {
 
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
@@ -152,57 +234,30 @@ public final class ProductServlet extends HttpServlet {
         writer.println("</head>");
         writer.println("<body>");
 
-        HttpURLConnection connection = (HttpURLConnection) new URL(targetURL).openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Authorization", "Token " + API_KEY);
-        connection.connect();
-        int status = connection.getResponseCode();
-        if (status == 200) {
-            try (InputStreamReader reader = new InputStreamReader(connection.getInputStream())) {
-                Type productType = new TypeToken<SimpleResponse<Product>>(){}.getType();
-                SimpleResponse<Product> results = gson.fromJson(reader, productType);
+        Product product = results.getResult();
+        writer.println("<table border=\"0\">");
 
-                if (results.getStatus() != 200) {
-                    writer.println("Request error:");
-                    writer.println(results.getMessage());
-                }
-                else {
-
-                    Product product = results.getResult();
-                    writer.println("<table border=\"0\">");
-
-                    writer.println("<tr>");
-                    writer.println("<td>ID</td>");
-                    writer.println("<td>" + product.getId() + "</td>");
-                    writer.println("</tr>");
-                    writer.println("<tr>");
-                    writer.println("<td>Name</td>");
-                    writer.println("<td>" + product.getName() + "</td>");
-                    writer.println("</tr>");
-                    writer.println("<tr>");
-                    writer.println("<td>Origin</td>");
-                    writer.println("<td>" + product.getOrigin() + "</td>");
-                    writer.println("</tr>");
-                    writer.println("<tr>");
-                    writer.println("<td>Category</td>");
-                    writer.println("<td>" + product.getCategory() + "</td>");
-                    writer.println("</tr>");
-                    writer.println("<tr>");
-                    writer.println("<td>Image</td>");
-                    writer.println("<td><img src=\"" + product.getThumbURL() + "\"></td>");
-                    writer.println("</tr>");
-                    writer.println("</table>");
-                }
-            }
-            catch (IOException e) {
-                writer.println("Error retrieving data:");
-                writer.println(e.getLocalizedMessage());
-            }
-        }
-        else {
-            writer.println("Error connecting to LCBO server:");
-            writer.println(connection.getResponseMessage());
-        }
+        writer.println("<tr>");
+        writer.println("<td>ID</td>");
+        writer.println("<td>" + product.getId() + "</td>");
+        writer.println("</tr>");
+        writer.println("<tr>");
+        writer.println("<td>Name</td>");
+        writer.println("<td>" + product.getName() + "</td>");
+        writer.println("</tr>");
+        writer.println("<tr>");
+        writer.println("<td>Origin</td>");
+        writer.println("<td>" + product.getOrigin() + "</td>");
+        writer.println("</tr>");
+        writer.println("<tr>");
+        writer.println("<td>Category</td>");
+        writer.println("<td>" + product.getCategory() + "</td>");
+        writer.println("</tr>");
+        writer.println("<tr>");
+        writer.println("<td>Image</td>");
+        writer.println("<td><img src=\"" + product.getThumbURL() + "\"></td>");
+        writer.println("</tr>");
+        writer.println("</table>");
 
         writer.println("</body>");
         writer.println("</html>");
